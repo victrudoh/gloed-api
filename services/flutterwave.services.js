c57d0543e07618e77bb151222b2c5a32f6c737f7
Victory <victrudoh@gmail.com>
2023-01-17 17:48:11 +0100

    switched flutterwave to live keys

--- services/flutterwave.services.js
+++ services/flutterwave.services.js
@@ -5,47 +5,47 @@ require("dotenv").config();
 const Flutterwave = require("flutterwave-node-v3");
 
 const baseURL = process.env.FLUTTERWAVE_BASE_URL;
-// const FLW_pubKey = process.env.FLUTTERWAVE_PUBLIC_KEY;
-// const FLW_secKey = process.env.FLUTTERWAVE_SECRET_KEY;
+const FLW_pubKey = process.env.FLUTTERWAVE_PUBLIC_KEY;
+const FLW_secKey = process.env.FLUTTERWAVE_SECRET_KEY;
 
 // TEST Mode
-const FLW_pubKey = process.env.FLUTTERWAVE_TEST_PUBLIC_KEY;
-const FLW_secKey = process.env.FLUTTERWAVE_TEST_SECRET_KEY;
+// const FLW_pubKey = process.env.FLUTTERWAVE_TEST_PUBLIC_KEY;
+// const FLW_secKey = process.env.FLUTTERWAVE_TEST_SECRET_KEY;
 
 const flw = new Flutterwave(FLW_pubKey, FLW_secKey);
 
 const options = {
-  timeout: 1000 * 60,
-  headers: {
-    "content-type": "application/json",
-    Authorization: `Bearer ${FLW_secKey}`,
-  },
+    timeout: 1000 * 60,
+    headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${FLW_secKey}`,
+    },
 };
 
-const initiateTransaction = async (payload) => {
-  try {
-    const response = await axios.post(`${baseURL}/payments`, payload, options);
-    console.log(`Bearer ${FLW_secKey}`);
-    return response.data.data.link;
-  } catch (err) {
-    console.log(err);
-  }
+const initiateTransaction = async(payload) => {
+    try {
+        const response = await axios.post(`${baseURL}/payments`, payload, options);
+        console.log(`Bearer ${FLW_secKey}`);
+        return response.data.data.link;
+    } catch (err) {
+        console.log(err);
+    }
 };
 
-const verifyTransaction = async (id) => {
-  try {
-    const response = await axios.get(
-      `${baseURL}/transactions/${id}/verify`,
-      options
-    );
-    console.log("verify: ", response.data);
-    return response.data.data;
-  } catch (err) {
-    console.log(err);
-  }
+const verifyTransaction = async(id) => {
+    try {
+        const response = await axios.get(
+            `${baseURL}/transactions/${id}/verify`,
+            options
+        );
+        console.log("verify: ", response.data);
+        return response.data.data;
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 module.exports = {
-  initiateTransaction,
-  verifyTransaction,
+    initiateTransaction,
+    verifyTransaction,
 };
